c7c919c1e59d1c254ba08545d1b4b88bdc095a5b
Mikami <Mikami9700@users.noreply.github.com>
2020-09-12 07:58:40 +0300

    one

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,34 +1,24 @@
 <template>
-  <div class="hello">
-    <h1>{{ title }}</h1>
+  <div>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "BookList",
-  data: {},
+  data() {
+    return {
+      title: "All Books",
+    };
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 h1,
 h2 {
   font-weight: normal;
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
